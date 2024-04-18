474ae016ee51407b53fdd0b67f8ddd2e3f5dc277
Ruan_Bertin <ruanbertin@hotmail.com>
2024-04-18 18:46:22 -0300

    feat: instalando os pacotes axios, skeleton e react query

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,14 +1,25 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from './Pets.module.css'
+import { Card } from "../../components/common/Card";
+// import { getPets } from "../../services/pets/getPets";
+// import { Skeleton } from "../../components/common/Skeleton";
 
 export function Pets(){
+    // const [pets, setPets] = useState([])
+    useEffect(() => {
+        
+    }, [])
     return (
     <Grid>
         <div className={styles.container}>
             <Header />
-            <Link to='/pets/20'>Ir para listagem</Link>
+            <main className={styles.list}>
+                <Card href="/pets/1" text="nina" thumb=""/>
+                <Card href="/pets/2" text="bob" thumb=""/>
+                <Card href="/pets/3" text="jão" thumb=""/>
+            </main>
         </div>
     </Grid>
     )
