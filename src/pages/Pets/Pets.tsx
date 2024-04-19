6ee7e0dfb12c060bbc7629fe74e8db987b74e979
Ruan_Bertin <ruanbertin@hotmail.com>
2024-04-19 05:29:48 -0300

    feat: criando a paginação da listagem de pets

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,25 +1,51 @@
-import { useEffect, useState } from "react";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from './Pets.module.css'
 import { Card } from "../../components/common/Card";
-// import { getPets } from "../../services/pets/getPets";
-// import { Skeleton } from "../../components/common/Skeleton";
+import { getPets } from "../../services/pets/getPets";
+import { Skeleton } from "../../components/common/Skeleton";
+import { useQuery } from "@tanstack/react-query";
+import { Pagination } from "../../components/common/Pagination";
+import { useSearchParams } from "react-router-dom";
 
 export function Pets(){
-    // const [pets, setPets] = useState([])
-    useEffect(() => {
-        
-    }, [])
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const urlParams = {
+        page: searchParams.get('page') ? Number(searchParams.get('page')) : 1
+    }
+
+    function changePage(page: number){
+        setSearchParams((params) => {
+            params.set('page', String(page))
+            return params
+        })
+    }
     return (
     <Grid>
         <div className={styles.container}>
             <Header />
+            {
+                isLoading && (
+                <Skeleton containerClassName={styles.skeleton} count={10}/>
+            )}
             <main className={styles.list}>
-                <Card href="/pets/1" text="nina" thumb=""/>
-                <Card href="/pets/2" text="bob" thumb=""/>
-                <Card href="/pets/3" text="jão" thumb=""/>
+                {data?.items?.map((pet) => (
+                    <Card 
+                    key={pet.id} 
+                    href={`/pet/${pet.id}`} 
+                    text={pet.name} 
+                    thumb={pet.photo} />
+                ))}
             </main>
+                
+            {   
+                data?.currentPage && <Pagination 
+                currentPage={data.currentPage}
+                totalPages={data.totalPages} 
+                onPageChange={(number) => changePage(number)}/>
+            }
+
         </div>
     </Grid>
     )
