68c0373b9276ea2c61b4d2c8ec16980e50634c58
Ruan_Bertin <ruanbertin@hotmail.com>
2024-04-19 23:35:48 -0300

    feat: cirando os filtros de listagem

---
+++ src/pages/Pets/Pets.constants.ts
@@ -0,0 +1,69 @@
+interface IFilterColumns {
+    name: 'gender' | 'size' | 'type'
+    title: string
+    options: { value: string; text: string }[]
+}
+
+export const filterColumns: IFilterColumns[] = [
+    {
+        name: 'type',
+        title: 'Espécie',
+        options: 
+        [
+            {
+                value: '',
+                text: 'Todos',
+            },
+            {
+                value: 'Cachorro',
+                text: 'Cachorros',
+            },
+            {
+                value: 'Gato',
+                text: 'Gatos',
+            },
+        ],
+    },
+    {
+        name: 'size',
+        title: 'Porte',
+        options: 
+        [
+            {
+            value: '',
+            text: 'Todos',
+            },
+            {
+            value: 'Pequeno',
+            text: 'Pequeno',
+            },
+            {
+            value: 'Médio',
+            text: 'Médio',
+            },
+            {
+            value: 'Grande',
+            text: 'Grande',
+            },
+        ],
+    },
+    {
+        name: 'gender',
+        title: 'Sexo',
+        options: 
+        [
+            {
+            value: '',
+            text: 'Todos',
+            },
+            {
+            value: 'Feminino',
+            text: 'Feminino',
+            },
+            {
+            value: 'Masculino',
+            text: 'Masculino',
+            },
+        ],
+    },
+]
--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -2,51 +2,102 @@ import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from './Pets.module.css'
 import { Card } from "../../components/common/Card";
-import { getPets } from "../../services/pets/getPets";
 import { Skeleton } from "../../components/common/Skeleton";
-import { useQuery } from "@tanstack/react-query";
 import { Pagination } from "../../components/common/Pagination";
 import { useSearchParams } from "react-router-dom";
+import { usePetList } from "../../hooks/usePetList";
+import { Select } from "../../components/common/Select";
+import { Button } from "../../components/common/Button";
+import { filterColumns } from "./Pets.constants";
+import { FormEvent } from "react";
+import { GetPetsRequest } from "../../interfaces/pet";
 
-export function Pets(){
+export function Pets() {
     const [searchParams, setSearchParams] = useSearchParams()
 
     const urlParams = {
-        page: searchParams.get('page') ? Number(searchParams.get('page')) : 1
+        page: searchParams.get('page') ? Number(searchParams.get('page')) : 1,
+        type: searchParams.get('type') ?? '',
+        size: searchParams.get('size') ?? '',
+        gender: searchParams.get('gender') ?? '',
     }
 
-    function changePage(page: number){
+    const { data, isLoading } = usePetList(urlParams)
+
+    function changePage(page: number) {
         setSearchParams((params) => {
             params.set('page', String(page))
             return params
         })
     }
+
+    function getFormValue(form: HTMLFormElement) {
+        const formData = new FormData(form)
+        return Object.fromEntries(formData)
+    }
+
+    function updateSearchParams(urlParams: GetPetsRequest) {
+        const fields: (keyof GetPetsRequest)[] = ['type', 'size', 'gender']
+        const newParams = new URLSearchParams()
+    
+        fields.forEach((field) => {
+            if (urlParams[field]) {
+            newParams.set(field, String(urlParams[field]))
+            }
+        })
+        newParams.set('page', '1')
+    
+        return newParams
+    }
+
+    function applyFilters(event: FormEvent) {
+        event.preventDefault()
+    
+        const formValues = getFormValue(event.target as HTMLFormElement)
+        const newSearchParams = updateSearchParams(formValues)
+    
+        setSearchParams(newSearchParams)
+    }
     return (
-    <Grid>
-        <div className={styles.container}>
+        <Grid>
+            <div className={styles.container}>
             <Header />
-            {
-                isLoading && (
-                <Skeleton containerClassName={styles.skeleton} count={10}/>
+            <form className={styles.filters} onSubmit={applyFilters}>
+                <div className={styles.columns}>
+                {filterColumns.map((filter) => (
+                    <div key={filter.name} className={styles.column}>
+                    <Select
+                        label={filter.title}
+                        defaultValue={urlParams[filter.name]}
+                        name={filter.name}
+                        options={filter.options}
+                    />
+                    </div>
+                ))}
+            </div>
+            <Button type="submit">Buscar</Button>
+            </form>
+            {isLoading && (
+                <Skeleton containerClassName={styles.skeleton} count={10} />
             )}
             <main className={styles.list}>
                 {data?.items?.map((pet) => (
-                    <Card 
-                    key={pet.id} 
-                    href={`/pet/${pet.id}`} 
-                    text={pet.name} 
-                    thumb={pet.photo} />
+                <Card
+                    key={pet.id}
+                    href={`/pet/${pet.id}`}
+                    text={pet.name}
+                    thumb={pet.photo}
+                />
                 ))}
             </main>
-                
-            {   
-                data?.currentPage && <Pagination 
+            {data?.currentPage && (
+                <Pagination
                 currentPage={data.currentPage}
-                totalPages={data.totalPages} 
-                onPageChange={(number) => changePage(number)}/>
-            }
-
-        </div>
-    </Grid>
-    )
-}
+                totalPages={data.totalPages}
+                onPageChange={(number) => changePage(number)}
+                />
+            )}
+            </div>
+        </Grid>
+        )
+    }
